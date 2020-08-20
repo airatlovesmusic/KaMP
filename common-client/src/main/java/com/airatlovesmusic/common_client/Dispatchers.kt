45c8c942b92b97f26c17086f4c47c5085b7faf07
airatlovesmusic <airatlovesmusic@gmail.com>
2020-08-20 23:08:18 +0300

    Added coroutines module for jvm platforms + Updated dependencies

---
+++ common-client/src/main/java/com/airatlovesmusic/common_client/Dispatchers.kt
@@ -0,0 +1,6 @@
+package com.airatlovesmusic.common_client
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+internal expect val ApplicationDispatcher: CoroutineDispatcher
+internal expect val UIDispatcher: CoroutineDispatcher
