64c94798136646b184f490667df24fd8fea2276c
airatlovesmusic <airat@rcsmail.ru>
2020-09-25 16:54:11 +0300

    [Backend] configuration fixes

--- backend/src/main/java/com/airatlovesmusic/backend/Main.kt
+++ backend/src/main/java/com/airatlovesmusic/backend/Main.kt
@@ -1,10 +1,10 @@
 package com.airatlovesmusic.backend
 
-import io.ktor.server.engine.commandLineEnvironment
+import io.ktor.application.*
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
     val port = System.getenv("PORT")?.toInt() ?: 23567
-    embeddedServer(Netty, port){}.start(true)
+    embeddedServer(factory = Netty, port = port, module = Application::module).start()
 }
