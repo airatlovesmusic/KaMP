544237c06a24da1a7fd868f21ae261df1263ca50
airatlovesmusic <airat@rcsmail.ru>
2020-09-27 22:57:15 +0300

    Backend startup config change

--- backend/src/main/java/com/airatlovesmusic/backend/Main.kt
+++ backend/src/main/java/com/airatlovesmusic/backend/Main.kt
@@ -6,5 +6,9 @@ import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
     val port = System.getenv("PORT")?.toInt() ?: 23567
-    embeddedServer(factory = Netty, port = port, module = Application::module).start()
+    embeddedServer(
+        factory = Netty,
+        port = port,
+        module = Application::module
+    ).start(wait = true)
 }
