45b9cc52776d63b3f4498ff035d623f2044cab7d
airatlovesmusic <airat@rcsmail.ru>
2020-09-27 23:09:38 +0300

    Add json serializer

--- backend/src/main/java/com/airatlovesmusic/backend/Server.kt
+++ backend/src/main/java/com/airatlovesmusic/backend/Server.kt
@@ -1,10 +1,25 @@
 package com.airatlovesmusic.backend
 
+import com.google.gson.GsonBuilder
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.GsonConverter
+import io.ktor.http.ContentType
 import io.ktor.routing.*
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        register(
+            ContentType.Application.Json,
+            GsonConverter(
+                GsonBuilder()
+                    .setPrettyPrinting()
+                    .serializeNulls()
+                    .create()
+            )
+        )
+    }
     install(Routing) {
         article()
     }
